51522acbc4a0690e1c4b5182e650677c2faa892c
Uli Bruckdorfer <uli@bruckdorfer-sr.de>
2017-07-28 09:47:56 +0200

    add more configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,17 @@ CFG_FILE=/usr/local/etc/mfs/mfsmetalogger.cfg
 
 echo "Set master host as $MFSM_MASTERHOST"
 sed -i "s/^MASTER_HOST = .*\|# MASTER_HOST = mfsmaster/MASTER_HOST = $MFSM_MASTERHOST/g" $CFG_FILE
+echo "Set backlogs to $MFSM_BACKLOG"
+sed -i "s/^BACK_LOGS = .*|# BACK_LOGS = .*/BACK_LOGS = $MFSM_BACKLOGS/g" $CFG_FILE
+echo "Set download frequency to $MFSM_DLFREQ"
+if [ $(($MFSM_BACKLOGS/2)) -gt $MFSM_DLFREQ ]
+then
+	sed -i "s/^META_DOWNLOAD_FREQ = .*|# META_DOWNLOAD_FREQ = .*/META_DOWNLOAD_FREQ = $MFSM_DLFREQ/g" $CFG_FILE
+else
+	echo "Download frequency is to high, it should be at most $(($MFSM_BACKLOGS/2))"
+	echo "Setting download frequency to $((($MFSM_BACKLOGS/2)-1))"
+	sed -i "s/^META_DOWNLOAD_FREQ = .*|# META_DOWNLOAD_FREQ = .*/META_DOWNLOAD_FREQ = $((($MFSM_BACKLOGS/2)-1))/g" $CFG_FILE
+fi
 
 echo ""
-echo "here we go..."
 mfsmetalogger -d
